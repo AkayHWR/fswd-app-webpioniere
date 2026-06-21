cd54c8fb5c39339eb4a0f29a361c7ed67ac627f5
Karl Tebbe <s_tebbe24@stud.hwr-berlin.de>
2026-06-21 22:17:39 +0200

    add error 404

--- app.py
+++ app.py
@@ -510,3 +510,9 @@ def api_questions():
     con = db.get_db_con()
     questions = con.execute('SELECT * FROM question ORDER BY id DESC').fetchall()
     return jsonify([dict(question) for question in questions])
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+
+    return render_template('404.html'), 404
