54741c9ae7014c885f8a5ba3fbf1184f00b3aee6
Karl Tebbe <s_tebbe24@stud.hwr-berlin.de>
2026-06-21 22:21:00 +0200

    add error 500

--- app.py
+++ app.py
@@ -516,3 +516,9 @@ def api_questions():
 def page_not_found(e):
 
     return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    
+    return render_template('500.html'), 500
